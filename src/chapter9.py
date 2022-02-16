90fdb55cc5d60951dbc0862a4aad2915615c5bbb
Jose-Araujo <jose.vinicius450@gmail.com>
2022-02-16 02:10:40 -0300

    Atualizando pastas com arquivos do capitulo 9, o projeto e a pasta de haarcascade

---
+++ src/chapter9.py
@@ -0,0 +1,16 @@
+
+from cv2 import cv2
+
+#FACE DETECTION
+
+faceCascade = cv2.CascadeClassifier("../haarcascades/haarcascade_frontalface_default.xml")
+img = cv2.imread("../imgs/Lenna.png")
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = faceCascade.detectMultiScale(imgGray, 1.1, 4)
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+cv2.imshow("Result", img)
+cv2.waitKey(0)
